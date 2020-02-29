ff6df7fb64ee09a2ae0c34a4e56db33e97e477da
electronicheart <shenzhendemail@163.com>
2020-02-29 11:37:58 +0800

    配置项目

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastclick from './fastclick'
+import './assets/styles/border.css'
+// 解决1像素边框问题
+import './assets/styles/reset.css'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 new Vue({
   router,
