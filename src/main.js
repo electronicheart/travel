9ef043a988db5bf3dfd1f3d4ad87c0168af12903
electronicheart <shenzhendemail@163.com>
2020-03-02 16:40:24 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastclick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/border.css'
 // 解决1像素边框问题
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <HomeHeader />
+    <HomeSwiper />
   </div>
 </template>
 
 <script>
 import HomeHeader from '@/components/home/Header'
+import HomeSwiper from '@/components/home/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
