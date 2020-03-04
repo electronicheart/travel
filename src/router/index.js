989341fff5789949d775c8b957048b77106cc08b
electronicheart <shenzhendemail@163.com>
2020-03-04 16:13:39 +0800

    city router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import City from '../views/City.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: 'home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/city',
+    name: 'City',
+    component: City
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
---
+++ src/views/City.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <CityHeader />
+  </div>
+</template>
+
+<script>
+import CityHeader from '@/components/city/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
